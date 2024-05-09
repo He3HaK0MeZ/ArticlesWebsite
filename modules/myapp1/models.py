24d1da3972d03b9e47e360a98b7d06d8c4660694
He3HaK0MeZ <egor-shestakov-02@mail.ru>
2024-05-09 09:49:28 +0300

    Paginator + filtration

--- modules/myapp1/models.py
+++ modules/myapp1/models.py
@@ -56,6 +56,17 @@ class Article(models.Model):
     Модель постов для сайта
     """
 
+    class ArticleManager(models.Manager):
+        """
+        Кастомный менеджер для модели статей
+        """
+
+        def all(self):
+            """
+            Список статей (SQL запрос с фильтрацией для страницы списка статей)
+            """
+            return self.get_queryset().filter(status='published')
+
     STATUS_OPTIONS = (
         ('published', 'Опубликовано'),
         ('draft', 'Черновик')
@@ -80,6 +91,8 @@ class Article(models.Model):
                                 related_name='updater_posts', blank=True)
     fixed = models.BooleanField(verbose_name='Зафиксировано', default=False)
     category = TreeForeignKey('Category', on_delete=models.PROTECT, related_name='articles', verbose_name='Категория')
+
+    objects = ArticleManager()
 
     class Meta:
         db_table = 'app_articles'
--- modules/myapp1/views.py
+++ modules/myapp1/views.py
@@ -1,12 +1,24 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .models import Article, Category
+
+
+def articles_list(request):
+    articles = Article.objects.all()
+    paginator = Paginator(articles, per_page=2)
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+    context = {'page_obj': page_object}
+    return render(request, 'blog/articles_func_list.html', context)
 
 
 class ArticleListView(ListView):
     model = Article
     template_name = 'blog/articles_list.html'
     context_object_name = 'articles'
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
