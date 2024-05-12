64b12b2dd4c7cb1fa379c8017aa1978b1bcdc876
He3HaK0MeZ <egor-shestakov-02@mail.ru>
2024-05-12 14:34:10 +0300

    Feedback form

--- modules/system/forms.py
+++ modules/system/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm, PasswordResetForm
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import Profile, Feedback
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -157,3 +157,21 @@ class UserSetNewPasswordForm(SetPasswordForm):
                 'class': 'form-control',
                 'autocomplete': 'off'
             })
+
+
+class FeedbackCreateForm(forms.ModelForm):
+    """
+    Форма отправки обратной связи
+    """
+
+    class Meta:
+        model = Feedback
+        fields = ('subject', 'email', 'content')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Обновление стилей формы
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control', 'autocomplete': 'off'})
